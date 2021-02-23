b586e9614886beee183d4e341084a94f51fc31be
linasmk <linas.mackonis@gmail.com>
2021-02-23 11:06:41 +0200

    Initialise esbuild-wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect } from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
   const onClick = () => {
     console.log(input);
   };
+  useEffect(() => {
+    startService();
+  }, []);
+
   return (
     <div>
       <textarea
